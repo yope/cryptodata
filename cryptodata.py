d23ad138e8a4d423c9a5e9d4a8c02a6d41697b50
David Jander <djander@gmail.com>
2018-08-17 17:44:30 +0200

    cryptodata.py: Add more strategy back-testing functionality
    Signed-off-by: David Jander <djander@gmail.com>

--- cryptodata.py
+++ cryptodata.py
@@ -52,28 +52,56 @@ class Candle:
 class BaseStrategy:
 	def __init__(self, candles, pyramiding=1):
 		self.candles = candles
-		self.capital = 1000
+		self.startcapital = self.capital = 1000
 		self.position = 0
-		self.pfrac = 0.2
+		self.equity = self.capital
+		self.trades = []
+		self.pfrac = 0.5
 		self.pyramiding = pyramiding
 		self.longshort = 0
+		self.winners = 0
+		self.losers = 0
+		self.total_win = 0
+		self.total_loss = 0
+
+	def add_trade(self, c, typ):
+		price = c.hl2()
+		x = self.pfrac * self.capital
+		if typ == "SHORT":
+			if self.position > 0: # Close long position before going short
+				x += self.position * price
+			self.capital += x
+			self.position -= x / price
+		elif typ == "LONG":
+			if self.position < 0: # Close short position before going long
+				x -= self.position * price
+			self.capital -= x
+			self.position += x / price
+		else:
+			print("ERROR: Unknown trade type {!r}".format(typ))
+			return
+		eq = self.capital + self.position * price
+		delta = eq - self.equity
+		self.trades.append((typ, eq, delta))
+		if delta > 0:
+			self.winners += 1
+			self.total_win += delta
+		elif delta < 0:
+			self.losers += 1
+			self.total_loss += delta
+		self.equity = eq
+		c.add_annotation(typ)
 
 	def go_long(self, c):
 		if self.longshort >= self.pyramiding:
 			return
-		x = self.pfrac * self.capital
-		self.capital -= x
-		self.position += x / c.hl2()
-		c.add_annotation("LONG")
+		self.add_trade(c, "LONG")
 		self.longshort += 1
 
 	def go_short(self, c):
-		if self.longshort <= -self.pyramiding:
+		if self.longshort <= -(self.pyramiding - 1):
 			return
-		x = self.pfrac * self.capital
-		self.capital += x
-		self.position -= x / c.hl2()
-		c.add_annotation("SHORT", above=True)
+		self.add_trade(c, "SHORT")
 		self.longshort -= 1
 
 	def run(self):
@@ -136,8 +164,16 @@ if __name__ == "__main__":
 	s.run()
 	for c in candles:
 		print(repr(c))
+	print("Trades:")
+	for i in range(len(s.trades)):
+		t = s.trades[i]
+		print("  Trade {:3d}: type: {:5s}, equity: {:5.2f}, delta: {:3.2f}".format(i, t[0], t[1], t[2]))
 	print("Strategy performance:")
-	print("position at end: {}".format(s.position))
-	print("capital at end:  {}".format(s.capital))
-	print("Closing all positions: {}".format(s.capital + s.position * candles[-1].close))
-
+	print("  position at end: {}".format(s.position))
+	print("  capital at end:  {}".format(s.capital))
+	print("  Closing all positions: {:5.2f}".format(s.capital + s.position * candles[-1].close))
+	print("  Number of losing trades: {}".format(s.losers))
+	print("  Number of winning trades:{}".format(s.winners))
+	print("  Total capital lost in losing trades:{:5.2f}".format(s.total_loss))
+	print("  Total capital won in winning trades:{:5.2f}".format(s.total_win))
+	print("  Total pnl:{:5.2f}".format(s.equity - s.startcapital))
